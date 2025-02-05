d76fe9458590b112868be1d92e50d1d615ef226f
Ryan <rkerry1@gmail.com>
2025-02-05 15:59:23 +0000

    fix: firebase deployment.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,16 @@
 #!bin/sh -x
 
-# Output versions.
-hugo version
-
 # Retrieve the input arguments/parameters.
-FIREBASE_DEPLOY_TOKEN=$1
-ALIAS=${2:-alias}
-HUGO_PARAMS=${3:-hugo-params}
+FIREBASE_PROJECT_NAME=$1
+FIREBASE_DEPLOY_TOKEN=$2
+ALIAS=${3:-alias}
+HUGO_PARAMS=${4:-hugo-params}
 
 # Build the Hugo site.
 hugo $HUGO_PARAMS
 
 # Publish to Google Firebase.
+firebase use $FIREBASE_PROJECT_NAME
 firebase use --token $FIREBASE_DEPLOY_TOKEN $ALIAS
 firebase deploy -m "
   Successful Deployment: 
