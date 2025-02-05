8108e89fe47b806c3a2f0b395f8e9f30cd9b3466
Ryan <rkerry1@gmail.com>
2025-02-05 16:12:55 +0000

    fix: alias and params.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,15 @@
 #!bin/sh -x
 
 # Retrieve the input arguments/parameters.
-FIREBASE_PROJECT_NAME=$1
-FIREBASE_DEPLOY_TOKEN=$2
+FIREBASE_DEPLOY_TOKEN=${1:-firebase-token}
+ALIAS=${2:-alias}
 HUGO_PARAMS=${3:-hugo-params}
 
 # Build the Hugo site.
 hugo $HUGO_PARAMS
 
 # Publish to Google Firebase.
-firebase use $FIREBASE_PROJECT_NAME
-firebase use --token $FIREBASE_DEPLOY_TOKEN
+firebase use --token $FIREBASE_DEPLOY_TOKEN $ALIAS
 firebase deploy -m "
   Successful Deployment: 
   Event: $GITHUB_EVENT_NAME,
