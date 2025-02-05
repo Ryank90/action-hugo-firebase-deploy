f0938997320d5219dca6b0c7ea6dff478c0af0bd
Ryan <rkerry1@gmail.com>
2025-02-05 23:05:27 +0000

    chore: clean up the removal of auth method from token to service account.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,20 @@
 #!bin/sh -x
 
 # Retrieve the input arguments/parameters.
-FIREBASE_DEPLOY_TOKEN=${1:-firebaseToken}
-FIREBASE_SERVICE_ACCOUNT=${2:-firebaseServiceAccount}
-PROJECT_ID=${3:-projectId}
-HUGO_PARAMS=${4:-hugoParams}
+FIREBASE_SERVICE_ACCOUNT=${1:-firebaseServiceAccount}
+PROJECT_ID=${2:-projectId}
+HUGO_PARAMS=${3:-hugoParams}
 
 # Build the Hugo site.
 hugo $HUGO_PARAMS
 
 # Publish to Google Firebase.
-
 echo $FIREBASE_SERVICE_ACCOUNT > credentials.json
 GOOGLE_APPLICATION_CREDENTIALS=credentials.json
 
-# firebase use --token $FIREBASE_DEPLOY_TOKEN $PROJECT_ID
 firebase deploy -m "
   Successful Deployment: 
   Event: $GITHUB_EVENT_NAME,
   Commit SHA: $GITHUB_SHA,
   User: $GITHUB_ACTOR
-" --non-interactive --token $FIREBASE_DEPLOY_TOKEN
+" --non-interactive --only hosting
