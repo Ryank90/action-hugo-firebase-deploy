6468b9c394f6195155a9fbc87f48f90aaecff73f
Ryan <rkerry1@gmail.com>
2025-02-05 16:03:59 +0000

    fix: remove alias reference.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,14 @@
 # Retrieve the input arguments/parameters.
 FIREBASE_PROJECT_NAME=$1
 FIREBASE_DEPLOY_TOKEN=$2
-ALIAS=${3:-alias}
-HUGO_PARAMS=${4:-hugo-params}
+HUGO_PARAMS=${3:-hugo-params}
 
 # Build the Hugo site.
 hugo $HUGO_PARAMS
 
 # Publish to Google Firebase.
 firebase use $FIREBASE_PROJECT_NAME
-firebase use --token $FIREBASE_DEPLOY_TOKEN $ALIAS
+firebase use --token $FIREBASE_DEPLOY_TOKEN
 firebase deploy -m "
   Successful Deployment: 
   Event: $GITHUB_EVENT_NAME,
